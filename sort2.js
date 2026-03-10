b8b60ed02151048125c405ae0dc8c152748a5e05
朱熙玥 <ihave8607@gmail.com>
2026-03-10 20:29:13 +0800

    排序二

--- sort2.js
+++ sort2.js
@@ -1,3 +1,15 @@
-// sort2.js
+$ git checkout add-sort-func2
+Edit the source code.
+// sort.js
+  var sortNumber = function (number) {
+      number.sort(function (a, b) {
+          if (a === b) {
+              return 0;
+          }
+          return a < b ? -1 : 1;
+      });
+  };
+
   var number = [19, 3, 81, 1, 24, 21];
+  sortNumber(number);
   console.log(number);
